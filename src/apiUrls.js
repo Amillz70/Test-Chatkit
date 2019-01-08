24ca72382f9c84eefb06c2c400aa9f69777b29bc
Amillz70 <adammiller7@yahoo.com>
2019-01-08 13:58:45 -0500

    Add new heroku url to apiUrl

--- src/apiUrls.js
+++ src/apiUrls.js
@@ -1,12 +1,7 @@
 let apiUrl
 const apiUrls = {
-<<<<<<< HEAD
   production: 'https://mighty-crag-77115.herokuapp.com/',
   development: 'http://localhost:3001'
-=======
-  production: 'https://evening-reef-19370.herokuapp.com',
-  development: 'http://localhost:7165'
->>>>>>> adam
 }
 
 if (window.location.hostname === 'localhost') {
